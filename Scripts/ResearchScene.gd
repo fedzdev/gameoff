8416a949ff219ca2a4eac2accef92d854d10ebae
fedz <janne-haa@hotmail.com>
2020-11-23 23:29:12 +0200

    research
    research

---
+++ Scripts/ResearchScene.gd
@@ -0,0 +1,30 @@
+extends "res://Scripts/ParentScene.gd"
+
+onready var label = get_node("Panel/CenterContainer/HBoxContainer/Label")
+
+var num_scroll = 0
+
+func _ready():
+	update_gun_text()
+
+
+func _on_RightButton_pressed():
+	num_scroll += 1
+	if num_scroll > 2:
+		num_scroll = 0
+	update_gun_text()
+
+func _on_LeftButton_pressed():
+	num_scroll -= 1
+	if num_scroll < 0:
+		num_scroll = 2
+	update_gun_text()
+
+
+func update_gun_text():
+	if num_scroll == 0:
+		label.text = "Pistol"
+	if num_scroll == 1:
+		label.text = "SMG"
+	if num_scroll == 2:
+		label.text = "Snipert"
